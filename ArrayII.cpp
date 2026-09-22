bf27e08920386b02834c11e8b460250277962134
Sangam Kumari <147520794+sangampandey21@users.noreply.github.com>
2026-09-22 22:43:38 +0530

    Update ArrayII.cpp

--- ArrayII.cpp
+++ ArrayII.cpp
@@ -25,8 +25,7 @@ private:
             result[x] = left[x];
         }
 
-        // Case 2: Contains the entire left interval, followed by a prefix of
-        // the right interval
+     
         for (int x = 0; x < k; x++) {
             result[(mulL * x) % k] += right[x];
         }
